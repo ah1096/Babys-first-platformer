72fe5f748e8b377961e982f815aada3063e48e9b
ah1096 <arh.10.96@gmail.com>
2023-04-30 21:27:02 +0000

    created method to parse array into 2d array

--- js/data/collisions.js
+++ js/data/collisions.js
@@ -1,4 +1,5 @@
- const collisionsLevel1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+const collisionsLevel1 = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 0,
     0, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0,
@@ -7,3 +8,16 @@
     0, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+//vv this method is apparently controversial because you shouldn't alter default JS objects
+Array.prototype.parse2D = function() {
+    const rows = []
+    for (let i = 0; i < this.length; i+=16) {
+        rows.push(this.slice(i, i + 16))
+    } //creates a 2d array from the data
+
+    return rows
+}
+
+const parsedCollisions = collisionsLevel1.parse2D()
+console.log(parsedCollisions)
