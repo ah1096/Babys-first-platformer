5cce10f77ba14881962c82fc28c7ebbbb35b1df7
ah1096 <arh.10.96@gmail.com>
2023-04-30 19:13:09 +0000

    continuous downward animation

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16 //1024
 canvas.height = 64 * 9 //576
 
-// fill rectangle with color
-c.fillStyle= 'white'
 //fillRect takes four arguments to draw a rectangle(x, y, width, height)
-c.fillRect(0, 0, canvas.width, canvas.height)
-
 
 //PLAYER//xxxxxxxxxxxxxxxxx//
-c.fillStyle = 'red'
-c.fillRect(100, 100, 100, 100)
-
 
 // create animation loop; call animate > call animate > call animte... until stopped
+let y = 100
+
 function animate() {
     window.requestAnimationFrame(animate)
-    console.log('go')
+    c.fillStyle= 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height) //redraw background to "clear" the canvas
+    c.fillStyle = 'red'
+    c.fillRect(100, y, 100, 100)
+    y++
 }
 
 animate()
