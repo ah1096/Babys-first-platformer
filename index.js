45816303b03f0efe3d76d67c91f8ecae793dd8da
ah1096 <arh.10.96@gmail.com>
2023-04-30 19:04:53 +0000

    create animate()

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ canvas.height = 64 * 9 //576
 c.fillStyle= 'white'
 //fillRect takes four arguments to draw a rectangle(x, y, width, height)
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+
+//PLAYER//xxxxxxxxxxxxxxxxx//
+c.fillStyle = 'red'
+c.fillRect(100, 100, 100, 100)
+
+
+// create animation loop; call animate > call animate > call animte... until stopped
+function animate() {
+    window.requestAnimationFrame(animate)
+    console.log('go')
+}
+
+animate()
