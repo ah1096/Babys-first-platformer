dec74bda2c115772f845b8e43329a645266e6138
ah1096 <arh.10.96@gmail.com>
2023-04-30 20:52:11 +0000

    add backgroundLevel1 image

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ canvas.width = 64 * 16 //1024
 canvas.height = 64 * 9 //576
 
 class Sprite {
-    constructor({position}) {
+    constructor({position, imageSrc}) { //using object syntax so that you can easily add position to each new instance
         this.position = position
         this.image = new Image()
-        this.image.src = ''
+        this.image.src = imageSrc
+    }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y) //render a JS Image() object + position on x and y axes
     }
 }
+
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/backgroundLevel1.png'
+})
 
 const player = new Player()
 
@@ -29,8 +40,8 @@ const keys = {
 
 function animate() {
     window.requestAnimationFrame(animate) // create animation loop
-    c.fillStyle= 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height) //redraw background to "clear" the canvas
+
+    backgroundLevel1.draw()
 
     player.velocity.x = 0
     if (keys.d.pressed) player.velocity.x = 5 //change this value to change player speed
