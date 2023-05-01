962b01e853134e1987d1d55748ecd3d858951b64
ah1096 <arh.10.96@gmail.com>
2023-05-01 10:23:28 +0000

    player has vertical collision detection

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,8 +4,8 @@ class Player {
         collisionBlocks =[]
     }) {
         this.position = {
-            x: 100,
-            y: 100,
+            x: 200,
+            y: 200,
         }
 
         this.velocity = {
@@ -13,8 +13,8 @@ class Player {
             y: 0,
         }
 
-        this.width = 100
-        this.height = 100
+        this.width = 25
+        this.height = 25
         this.sides = {
             bottom: this.position.y + this.height,
         }
@@ -32,7 +32,7 @@ class Player {
     // what properties within the player class should be altered over time?
     update() {
         this.position.x += this.velocity.x
-        //check for horizontal collisions
+        //check for HORIZONTAL collisions
         for (let i = 0; i < this.collisionBlocks.length; i++) {
             const collisionBlock = this.collisionBlocks[i] //check for collision between this one block and player's position
 
@@ -43,27 +43,47 @@ class Player {
                 this.position.y <= collisionBlock.position.y + collisionBlock.height // TOP of player vs BOTTOM of block
             ) {
                 //collision on x axis going to the left
-                if (this.velocity.x < - 1){
+                if (this.velocity.x < 0){
                     this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01 // move player to RIGHT side of block; 0.01 adds buffer between objects
                     break // break out of for loop bc it's inefficient to keep running it when collision detected
                 }
 
                 //collision on x axis going to the right
-                if (this.velocity.x > 1){
+                if (this.velocity.x > 0){
                     this.position.x = collisionBlock.position.x - this.width - 0.01
                     break
                 }
             }
         }
 
+        //apply gravity
+        this.velocity.y += this.gravity
         this.position.y += this.velocity.y
-        this.sides.bottom = this.position.y + this.height
 
-        //above bottom of canvas
-        if (this.sides.bottom + this.velocity.y < canvas.height){
-            this.velocity.y += this.gravity
-        } else {
-            this.velocity.y = 0
+        //check for VERTICAL collisions
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i]
+
+            //same as x axis; checks collisions on all sides of player
+            if (this.position.x <= collisionBlock.position.x + collisionBlock.width && // LEFT of player vs RIGHT of block
+                this.position.x + this.width >= collisionBlock.position.x && // RIGHT of player vs LEFT of block
+                this.position.y + this.height >= collisionBlock.position.y && // BOTTOM of player vs TOP of block
+                this.position.y <= collisionBlock.position.y + collisionBlock.height // TOP of player vs BOTTOM of block
+            ) {
+                //collision on y axis going UP
+                if (this.velocity.y < 0){
+                    this.velocity.y = 0 // added to prevent y velocity from increasing w/ added gravity; prevent player clipping through to bottom after jumping
+                    this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01 // TOP of player hits BOTTOM of block; push player down 0.01
+                    break 
+                }
+
+                //collision on y axis going DOWN
+                if (this.velocity.y > 0){
+                    this.velocity.y = 0
+                    this.position.y = collisionBlock.position.y - this.height - 0.01 //BOTTOM of player hits TOP of block; push player up 0.01
+                    break
+                }
+            }
         }
     }
 }
