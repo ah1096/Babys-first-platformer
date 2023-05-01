9f3544e612a0b06010369caa11c3a4f0deddb44f
ah1096 <arh.10.96@gmail.com>
2023-05-01 17:48:42 +0000

    modify w key to add door interaction

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,6 +1,20 @@
 window.addEventListener('keydown', (event) => {
     switch (event.key){
-        case 'w': //move player up
+        case 'w': //move player up + enter doors
+            for (let i = 0; i < doors.length; i++){
+                const door = doors[i]
+                if (
+                //player only activates door if player is in the MIDDLE of the door
+                player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width && //RIGHT of player <= RIGHT side of door
+                player.hitbox.position.x  >= door.position.x && //LEFT of player >= LEFT of door
+                
+                player.hitbox.position.y + player.hitbox.height >= door.position.y && 
+                player.hitbox.position.y <= door.position.y + door.height
+                ) {
+                    console.log('we are colliding')
+                    return 
+                }
+            }
             if (player.velocity.y === 0)
             player.velocity.y = -25 //adjust jump height here
             break
