d043065bcac44e966784be5488210fe57903209e
ah1096 <arh.10.96@gmail.com>
2023-05-01 11:33:35 +0000

    add player framerate; sprite now cropped

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,7 +1,7 @@
 class Player extends Sprite {
     // what properties are associated with each player?
-    constructor({collisionBlocks =[], imageSrc}) {
-        super({imageSrc}) //call Sprite's constructor
+    constructor({collisionBlocks =[], imageSrc, frameRate}) {
+        super({imageSrc, frameRate}) //call Sprite's constructor
         this.position = {
             x: 200,
             y: 200,
