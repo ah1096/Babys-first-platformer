fd8807e084e71174c3439c91953250039e752e28
ah1096 <arh.10.96@gmail.com>
2023-05-01 10:24:03 +0000

    change player jump height 20 -> 25

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -2,7 +2,7 @@ window.addEventListener('keydown', (event) => {
     switch (event.key){
         case 'w': //move player up
             if (player.velocity.y === 0)
-            player.velocity.y = -20 //adjust jump height here
+            player.velocity.y = -25 //adjust jump height here
             break
         case 'a': //move player left
             keys.a.pressed = true
