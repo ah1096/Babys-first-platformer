ddf2a145595cac0b11dd816d14743f8f3173d4a0
ah1096 <arh.10.96@gmail.com>
2023-05-01 10:53:10 +0000

    add width + height to Sprite instead of Player

--- js/classes/Sprite.js
+++ js/classes/Sprite.js
@@ -4,6 +4,8 @@ class Sprite {
         this.image = new Image()
         this.image.onload = () => {
             this.loaded = true
+            this.width = this.image.width
+            this.height = this.image.height
         }
         this.image.src = imageSrc
         this.loaded = false
